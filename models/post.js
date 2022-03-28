a3fc9cec8e59dd9814e76249203ba0437f68ca45
betterthem <betterthem@naver.com>
2022-03-28 21:30:39 +0900

    main publishing

---
+++ models/post.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const postSchema = new Schema({
+
+}, { versionKey : false });
+
+module.exports = mongoose.model('post', postSchema);
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 module.exports = function(app, Admin)
 {
+  // 어드민 계정가입 API
   app.post('/api/register', function(req, res) {
     var admin = new Admin();
     admin.admin_id = req.body.admin_id;
@@ -15,4 +16,13 @@ module.exports = function(app, Admin)
       res.json({result: 1});
     })
   });
+  
+  // 어드민 계정 데이터 API
+  app.get('/api/admins', function(req, res) {
+    Admin.find(function(err, data) {
+      if(err) return res.status(500).send({err: 'database failure'});
+      res.json(data);
+    })
+  });
+  
 }
